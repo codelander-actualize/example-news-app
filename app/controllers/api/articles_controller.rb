59a6a6bf14cc4b63eb8db222d2bb6077f083c6d1
dani <dani.zaghian@gmail.com>
2019-02-04 10:56:20 -0800

    adds http gem headers syntax

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,7 +1,9 @@
 class Api::ArticlesController < ApplicationController
 
 	def index
-		@articles = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:q]}&from=2019-01-04&sortBy=publishedAt&apiKey=#{ENV['API_KEY']}").parse["articles"]
+		@articles = HTTP.headers({
+			"X-Api-Key" => ENV["API_KEY"]
+		}).get("https://newsapi.org/v2/everything?q=#{params[:q]}&from=2019-01-04&sortBy=publishedAt").parse["articles"]
 		render 'index.json.jbuilder'
 	end
 
