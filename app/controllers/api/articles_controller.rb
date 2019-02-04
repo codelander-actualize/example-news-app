83165e695bb1641ecfe739368bf668db7613076b
dani <dani.zaghian@gmail.com>
2019-02-04 10:43:26 -0800

    adds env variables

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,7 +1,7 @@
 class Api::ArticlesController < ApplicationController
 
 	def index
-		@articles = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:q]}&from=2019-01-04&sortBy=publishedAt&apiKey=eda1b96b200f466b8bce1df354d74871").parse["articles"]
+		@articles = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:q]}&from=2019-01-04&sortBy=publishedAt&apiKey=#{ENV['API_KEY']}").parse["articles"]
 		render 'index.json.jbuilder'
 	end
 
